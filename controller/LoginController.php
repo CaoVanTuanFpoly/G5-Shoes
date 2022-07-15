6734b901fca3f36be668e161616a4a6855353618
pthieenlong <p.thieenlong.304@gmail.com>
2022-07-15 22:00:57 +0700

    login

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -2,11 +2,18 @@
 include '/Applications/XAMPP/xamppfiles/htdocs/G5-Shoes/Utils/Database.php';
 include '/Applications/XAMPP/xamppfiles/htdocs/G5-Shoes/Model/DAO/UserDAO.php';
 
+
+$userDAO = new UserDAO();
 $email = isset($_POST['email']) ? $_POST['email'] : 'error';
 $password = isset($_POST['password']) ? $_POST['password'] : 'error';
 
 
 if(isset($_POST['login'])) {
-    
+    $user = $userDAO->getUserByMail($email) != false ? $userDAO->getUserByMail($email) : 'error';
+    if($user->getPassword() == $password) {
+        $_SESSION['user'] = $user->getID();
+        echo 'success';
+    } else echo 'loi me may r';
+
 }
 ?>
--- model/DAO/UserDAO.php
+++ model/DAO/UserDAO.php
@@ -1,4 +1,5 @@
 <?php 
+include '/Applications/XAMPP/xamppfiles/htdocs/G5-Shoes/Model/User.php';
 class UserDAO {
     private $database;
     public function __construct() {
@@ -12,6 +13,22 @@ class UserDAO {
         } else {
             $query = $this->database->prepare('SELECT * FROM `khachhang` WHERE `khachhang`.`maKH` = ?');
             $query->bind_param('s', $id);
+
+            if($query->execute()) {
+                $result = $query->get_result();
+                if($result->num_rows > 0) {
+                    $user = $result->fetch_assoc();
+                    return new User($user['maKH'], $user['isAdmin'], $user['hoTen'], $user['ngaySinh'], $user['diaChi'], $user['mail'], $user['soDT'], $user['password'], $user['avata']);
+                } else return false;
+            } else return false;
+        }
+    }
+    public function getUserByMail($mail) {
+        if($this->database->connect_error) {
+            return false;
+        } else {
+            $query = $this->database->prepare('SELECT * FROM `khachhang` WHERE `khachhang`.`mail` = ?');
+            $query->bind_param('s', $mail);
 
             if($query->execute()) {
                 $result = $query->get_result();
